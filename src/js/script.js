8ca248d7219e140658f8e967af3c84b3ac83fa60
mizucode <mizurisysthensis3@gmail.com>
2023-04-19 06:22:47 +0700

    added jumbutron slider

---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+const wrapper = document.querySelector(".wrapper"),
+	carousel = document.querySelector(".carousel"),
+	images = document.querySelectorAll(".carousel-img"),
+	buttons = document.querySelectorAll(".button");
+
+let imageIndex = 1,
+	intervalid;
+
+const autoslide = () => {
+	intervalid = setInterval(() => slideImage(++imageIndex), 8000);
+};
+autoslide();
+
+const slideImage = () => {
+	imageIndex =
+		imageIndex === images.length
+			? 0
+			: imageIndex < 0
+			? images.length - 1
+			: imageIndex;
+	carousel.style.transform = `translate(-${imageIndex * 100}%)`;
+};
+
+const updateClick = (e) => {
+	clearInterval(intervalid);
+	imageIndex += e.target.id === "next" ? 1 : -1;
+	slideImage(imageIndex);
+	autoslide();
+};
+
+buttons.forEach((button) => button.addEventListener("click", updateClick));
+
+wrapper.addEventListener("mouseover", () => clearInterval(intervalid));
+wrapper.addEventListener("mouseleave", autoslide);
